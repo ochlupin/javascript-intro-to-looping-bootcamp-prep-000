276cdfeec91e2c80f2ede7e991c9d16ca0817d5b
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-08-13 13:41:14 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,10 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 0) {
+      array.push("I am a strange loop")
+    } else {
+      array.push(`I am ${i+1} strange loops.`)
+    }
+  }
+  return array;
+};
